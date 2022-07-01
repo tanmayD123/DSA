af40e8e6d6b7a554f03fec7e2930850a7a8b6576
Tanmay Dalal <70852116+tanmayD123@users.noreply.github.com>
2022-07-01 21:34:54 +0530

    Update reverseInteger.java

--- reverseInteger.java
+++ reverseInteger.java
@@ -14,6 +14,10 @@ public class ReverseInteger {
         while(n!=0)
         {
             int digit = n % 10;
+            
+            if( (ans > Integer.MAX_VALUE/10) || (ans < Integer.MIN_VALUE/10) )
+                return 0;
+            
             ans = (ans * 10) + digit;
             n = n / 10;
         }
